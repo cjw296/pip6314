4d9295a7ac2a4210ec1c74dac650e3844fa3cc58
Chris Withers <chris@simplistix.co.uk>
2019-03-03 20:20:59 +0000

    mininal reproducer

---
+++ README.rst
@@ -0,0 +1,8 @@
+To reproduce:
+
+```
+python3.7 -m venv ve
+source ve/bin/activate
+pip install -U pip
+pip install -e .
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup_kwargs = {
+    'name': 'pip6314',
+    'version': '0.0.0',
+    'install_requires': ['attrs>=18.2,<19.0'],
+}
+
+setup(**setup_kwargs)
